d082e214dcce3dc93e26e6203ed5b625064ab631
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2023-02-23 00:46:00 +0800

    updated bat file

--- telegraf.bat
+++ telegraf.bat
@@ -3,8 +3,13 @@ echo start
 
 rem change environment + start the server
 call "C:\Users\ASUS\anaconda3\Scripts\activate.bat" "C:\Users\ASUS\anaconda3\envs\opcua"
+
+rem add in the token
+call set INFLUX_TOKEN = 'API token copied from InfluxDB cloud'
+
+rem change this to the project directory
 call cd D:\github\OPC-UA-SENSORS-SIMULATOR
 start python run_opc_server.py
 
-rem link to cloud database (InfluxDB Cloud)
+rem start telegraf
 start telegraf --config "D:\github\OPC-UA-Sensors-Simulator\telegraf_opcua.conf"
